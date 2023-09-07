4eb82f3d6b663543adc16fc5ce5463084f0a9392
mrcjrc <mark.cavanagh1982@gmail.com>
2023-09-07 16:14:52 +1000

    Add files via upload

---
+++ Data_Junk.js
@@ -0,0 +1,170 @@
+function GetJunkData()
+{
+    return [
+        {
+            "Item": "Waste Acid",
+            "Keep": 50,
+            "Sell": 4
+        },
+        {
+            "Item": "Excess Adhesive",
+            "Keep": 200,
+            "Sell": 3
+        },
+        {
+            "Item": "Aluminum scrap",
+            "Keep": 100,
+            "Sell": 3
+        },
+        {
+            "Item": "Waste Antiseptic",
+            "Keep": 50,
+            "Sell": 2
+        },
+        {
+            "Item": "Raw Asbestos",
+            "Keep": 50,
+            "Sell": 3
+        },
+        {
+            "Item": "Ballistic Fiber Sc",
+            "Keep": 200,
+            "Sell": 8
+        },
+        {
+            "Item": "Black Titanium S",
+            "Keep": 1000000,
+            "Sell": 2
+        },
+        {
+            "Item": "Bone Shards",
+            "Keep": 50,
+            "Sell": 1
+        },
+        {
+            "Item": "Ceramic Scrap",
+            "Keep": 50,
+            "Sell": 1
+        },
+        {
+            "Item": "Circuits",
+            "Keep": 100,
+            "Sell": 3
+        },
+        {
+            "Item": "Raw Cloth",
+            "Keep": 100,
+            "Sell": 1
+        },
+        {
+            "Item": "Concrete Scrap",
+            "Keep": 100,
+            "Sell": 2
+        },
+        {
+            "Item": "Copper Scrap",
+            "Keep": 50,
+            "Sell": 3
+        },
+        {
+            "Item": "Cork Scrap",
+            "Keep": 50,
+            "Sell": 1
+        },
+        {
+            "Item": "Crystal Shards",
+            "Keep": 100,
+            "Sell": 4
+        },
+        {
+            "Item": "Raw Fertilizer",
+            "Keep": 50,
+            "Sell": 0.5
+        },
+        {
+            "Item": "Fiber Optics Bun",
+            "Keep": 100,
+            "Sell": 3
+        },
+        {
+            "Item": "Fiberglass Spool",
+            "Keep": 100,
+            "Sell": 2
+        },
+        {
+            "Item": "Loose Gears",
+            "Keep": 1000000,
+            "Sell": 2
+        },
+        {
+            "Item": "Glass Shard",
+            "Keep": 100,
+            "Sell": 1
+        },
+        {
+            "Item": "Gold Scrap",
+            "Keep": 50,
+            "Sell": 4
+        },
+        {
+            "Item": "Lead Scrap",
+            "Keep": 200,
+            "Sell": 2
+        },
+        {
+            "Item": "Raw Leather",
+            "Keep": 50,
+            "Sell": 2
+        },
+        {
+            "Item": "Nuclear Waste",
+            "Keep": 100,
+            "Sell": 3
+        },
+        {
+            "Item": "Waste Oil",
+            "Keep": 100,
+            "Sell": 2
+        },
+        {
+            "Item": "Molded Plastic",
+            "Keep": 100,
+            "Sell": 2
+        },
+        {
+            "Item": "Raw Rubber",
+            "Keep": 50,
+            "Sell": 1.5
+        },
+        {
+            "Item": "Loose Screws",
+            "Keep": 1000000,
+            "Sell": 4
+        },
+        {
+            "Item": "Silver Scrap",
+            "Keep": 50,
+            "Sell": 2
+        },
+        {
+            "Item": "Loose Spring",
+            "Keep": 1000000,
+            "Sell": 2
+        },
+        {
+            "Item": "Steel Scrap",
+            "Keep": 100,
+            "Sell": 0.5
+        },
+        {
+            "Item": "Ultracite Scrap",
+            "Keep": 1000000,
+            "Sell": 6
+        },
+        {
+            "Item": "Wood Scraps",
+            "Keep": 100,
+            "Sell": 0.5
+        }
+    ];
+}
